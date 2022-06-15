ed5ea9bb095caa8b879c9026275927ec1ca96d75
Antoine PRONNIER (Funix) <antoine.pronnier@gmail.com>
2022-06-15 04:22:47 +0300

    add port mysql

--- backup/backupMariadb.sh
+++ backup/backupMariadb.sh
@@ -7,7 +7,7 @@ find "$BACKUP_DATABASE_PATH_PACIFISTA" -type f -mtime +15 -delete
 find "$BACKUP_DATABASE_PATH_FUNIX" -type f -mtime +15 -delete
 
 #start backup mariadb db pacifista
-mysqldump -h "$MARIADB_ADDRESS" -u "$MARIADB_USER_PACIFISTA" -p"$MARIADB_PASSWORD_PACIFISTA" --all-databases | zip > "$BACKUP_DATABASE_PATH_PACIFISTA/mariadb-pacifista-$BACKUP_NAME.zip"
+mysqldump -h "$MARIADB_ADDRESS" -P "$MARIADB_PORT_PACIFISTA" -u "$MARIADB_USER_PACIFISTA" -p"$MARIADB_PASSWORD_PACIFISTA" --all-databases | zip > "$BACKUP_DATABASE_PATH_PACIFISTA/mariadb-pacifista-$BACKUP_NAME.zip"
 
 #start backup mariadb db funix
-mysqldump -h "$MARIADB_ADDRESS" -u "$MARIADB_USER_FUNIX" -p"$MARIADB_PASSWORD_FUNIX" --all-databases | zip > "$BACKUP_DATABASE_PATH_FUNIX/mariadb-funix-$BACKUP_NAME.zip"
+mysqldump -h "$MARIADB_ADDRESS" -P "$MARIADB_PORT_FUNIX" -u "$MARIADB_USER_FUNIX" -p"$MARIADB_PASSWORD_FUNIX" --all-databases | zip > "$BACKUP_DATABASE_PATH_FUNIX/mariadb-funix-$BACKUP_NAME.zip"
