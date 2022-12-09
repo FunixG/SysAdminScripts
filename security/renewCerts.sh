32a9c69bd92aadf3079d7ea8d74da76053976195
Antoine PRONNIER <44138938+FunixG@users.noreply.github.com>
2022-12-09 14:42:54 +0100

    Update renewCerts.sh
    Adding cloudflare access

--- security/renewCerts.sh
+++ security/renewCerts.sh
@@ -4,7 +4,7 @@ echo "Arrêt de apache2."
 systemctl stop apache2
 
 echo "Lancement du renouvellement des certificats."
-certbot renew
+certbot renew --dns-cloudflare --dns-cloudflare-credentials /root/.secrets/certbot/cloudflare.mydomain.ini
 
 echo "Relancement de apache2."
 systemctl start apache2
