f05706158c37763f88c5c3561a996ee35d6d0818
FunixG <antoine.pronnier@gmail.com>
2023-07-21 15:59:14 +0200

    fix cert renew

--- security/renewCerts.sh
+++ security/renewCerts.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 
-echo "Arrêt de apache2."
-systemctl stop apache2
-
 echo "Lancement du renouvellement des certificats."
 certbot renew --dns-cloudflare --dns-cloudflare-credentials /root/.secrets/certbot/cloudflare.mydomain.ini
-
-echo "Relancement de apache2."
-systemctl start apache2
