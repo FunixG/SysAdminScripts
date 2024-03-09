2282fa54cb82d00e0432c277e1531fc9205bfef4
Antoine PRONNIER <44138938+FunixG@users.noreply.github.com>
2024-03-09 11:06:29 +0100

    Update backupPostgres.sh

--- backup/backupPostgres.sh
+++ backup/backupPostgres.sh
@@ -6,8 +6,8 @@ BACKUP_NAME="$(date +'%d-%m-%Y_%H:%M:%S')"
 find "$PATH_NAS_BACKUP/backup-db" -type f -mtime +15 -delete
 
 #Create backup for postgresql databases
-PGPASSWORD=$PGPASSWORD_FUNIX pg_dump -h "$POSTGRESQL_FUNIX_ADDRESS" -p "$POSTGRESQL_FUNIX_PORT" -U "$POSTGRESQL_FUNIX_USER" -d funixproductions_db | zip > "$PATH_NAS_BACKUP/backup-db/postgresql-funixproductions-$BACKUP_NAME.zip"
-PGPASSWORD=$PGPASSWORD_FUNIX pg_dump -h "$POSTGRESQL_FUNIX_ADDRESS" -p "$POSTGRESQL_FUNIX_PORT" -U "$POSTGRESQL_FUNIX_USER" -d funixapi | zip > "$PATH_NAS_BACKUP/backup-db/postgresql-funixgaming-$BACKUP_NAME.zip"
+PGPASSWORD=$PGPASSWORD_FUNIXPROD pg_dump -h "$POSTGRESQL_FUNIXPROD_ADDRESS" -p "$POSTGRESQL_FUNIXPROD_PORT" -U "$POSTGRESQL_FUNIXPROD_USER" -d funixproductions_db | zip > "$PATH_NAS_BACKUP/backup-db/postgresql-funixproductions-$BACKUP_NAME.zip"
+PGPASSWORD=$PGPASSWORD_FUNIXGAMING pg_dump -h "$POSTGRESQL_FUNIXGAMING_ADDRESS" -p "$POSTGRESQL_FUNIXGAMING_PORT" -U "$POSTGRESQL_FUNIXGAMING_USER" -d funixgaming_db | zip > "$PATH_NAS_BACKUP/backup-db/postgresql-funixgaming-$BACKUP_NAME.zip"
 PGPASSWORD=$PGPASSWORD_PACIFISTA pg_dump -h "$POSTGRESQL_PACIFISTA_ADDRESS" -p "$POSTGRESQL_PACIFISTA_PORT" -U "$POSTGRESQL_PACIFISTA_USER" -d pacifista_db | zip > "$PATH_NAS_BACKUP/backup-db/postgresql-pacifista-$BACKUP_NAME.zip"
 
 #To import database follow this steps:
