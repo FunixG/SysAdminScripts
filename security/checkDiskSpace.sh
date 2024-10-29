9d1e043bfc982e2e9bc3e43e507b3b119e0998e9
Antoine PRONNIER <antoine.pronnier@gmail.com>
2024-10-29 13:04:41 +0100

    Fix

--- security/checkDiskSpace.sh
+++ security/checkDiskSpace.sh
@@ -9,4 +9,4 @@ TARGET_DIR="/dev/md3"
 # Calculate directory size (in human-readable format)
 DIR_SIZE=$(df -h "$TARGET_DIR")
 
-"$SCRIPT_PATH/../discord/sendMessageDiscord.sh" ":floppy_disk: **Espace disque:**\n$DIR_SIZE"
+"$SCRIPT_PATH/../discord/sendMessageDiscord.sh" ":floppy_disk: **Espace disque:** $DIR_SIZE"
