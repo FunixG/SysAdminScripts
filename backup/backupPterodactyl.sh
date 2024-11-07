bf31d4ca80f00d57696c442d4b12fb5c4041a711
Antoine PRONNIER <44138938+FunixG@users.noreply.github.com>
2024-11-07 13:32:34 +0100

    Update backupPterodactyl.sh

--- backup/backupPterodactyl.sh
+++ backup/backupPterodactyl.sh
@@ -23,10 +23,5 @@ find "$PATH_NAS_BACKUP/backup-servers" -type f -mtime +14 -delete
 #Create backup of pterodactyl
 send_message "Backup de Pterodactyl en cours..."
 
-ERROR=$(zip -r "$PATH_NAS_BACKUP/backup-servers/$BACKUP_NAME.zip" "$PTERODACTYL_PATH" 2>&1) 
-if [ $? -ne 0 ]; then
-    send_error_message "Erreur lors du backup de Pterodactyl : $ERROR"
-    exit 1
-else
-    send_success_message "Backup de Pterodactyl terminé !"
-fi
+zip -r "$PATH_NAS_BACKUP/backup-servers/$BACKUP_NAME.zip" "$PTERODACTYL_PATH"
+send_success_message "Backup de Pterodactyl terminé !"
